e163a8cb2a72e6e6ec08b966340273ea683e13a0
chabati-creator <batistischarles@gmail.com>
2021-03-04 15:02:37 +1300

    Formatting Windows Form
    Changing the background colour to light blue and adding a label 'Temperature Converter'

--- Temperature Convertor/Temperature Convertor/Form1.Designer.cs
+++ Temperature Convertor/Temperature Convertor/Form1.Designer.cs
@@ -29,13 +29,37 @@ namespace Temperature_Convertor
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(103, 230);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(326, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Temperature Converter";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(552, 529);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
 
